9937848052bab6ac383ec1a5e52d38eea5284c32
rigon <2412789408@qq.com>
2024-09-28 22:02:11 +0800

    add GORM

---
+++ GORM/chap1/intro.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Code  string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	db.Create(&Product{Code: "D42", Price: 100})
+
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code=?", "D42")
+
+	db.Model(&product).Update("Price", 200)
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+	db.Delete(&product, 1)
+}
---
+++ GORM/go.mod
@@ -0,0 +1,17 @@
+module gorm
+
+go 1.23.1
+
+require (
+	gorm.io/driver/sqlite v1.5.2
+	gorm.io/gorm v1.25.12
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.23 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
