3e1f00bee50d0bad01a5ecf45b27f78730ecb594
rigon <2412789408@qq.com>
2024-10-02 09:38:08 +0800

    fix bug

--- algo/practice/graph/graph_dfs.go
+++ algo/practice/graph/graph_dfs.go
@@ -2,13 +2,12 @@ package graph
 
 import (
 	. "practice/pkg"
-	"strconv"
 )
 
 func dfs(g *graphAdjList, visited map[Vertex]struct{}, res *[]Vertex, vet Vertex) {
 	*res = append(*res, vet)
 	visited[vet] = struct{}{}
-	for _, adjVet := g.adjList[vet] {
+	for _, adjVet := range g.adjList[vet] {
 		if _, isVisited := visited[adjVet]; !isVisited {
 			dfs(g, visited, res, adjVet)
 		}
