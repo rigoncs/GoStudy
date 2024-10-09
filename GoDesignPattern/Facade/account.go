496992d93264e112d0b2daa07f174a1bac772453
rigon <2412789408@qq.com>
2024-10-09 20:00:40 +0800

    add facade

---
+++ GoDesignPattern/Facade/account.go
@@ -0,0 +1,19 @@
+package facade
+
+import "fmt"
+
+type Account struct {
+	name string
+}
+
+func newAccount(accountName string) *Account {
+	return &Account{name: accountName}
+}
+
+func (a *Account) checkAccount(accountName string) error {
+	if a.name != accountName {
+		return fmt.Errorf("Account name is incorrect.")
+	}
+	fmt.Println("Account Verified")
+	return nil
+}
